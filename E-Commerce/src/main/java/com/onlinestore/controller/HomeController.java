8c362be8377e58b2d47ee5ea847e31e0fc8da372
bw_flow09 <viethoang.ma1009@gmail.com>
2020-12-17 20:21:16 +0700

    3rd Commit

--- E-Commerce/src/main/java/com/onlinestore/controller/HomeController.java
+++ E-Commerce/src/main/java/com/onlinestore/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.onlinestore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,10 @@ public class HomeController {
 	@RequestMapping("/myAccount")
 	public String myAccount() {
 		return "account";
-	}
+	}/*
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("aa-myaccount-login",true);
+		return "account";
+	}*/
 }
