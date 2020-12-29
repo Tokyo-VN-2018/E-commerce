102c648c117cbe3949784d28049ce313f232e155
Nguyen Minh Hieu <75939521+kinglork932000@users.noreply.github.com>
2020-12-29 21:02:31 +0700

    Add files via upload

---
+++ SQL/product_2.sql
@@ -0,0 +1,26 @@
+USE ECOMMERCE;
+INSERT INTO PRODUCTS (CATEGORY_ID,PRODUCT_NAME,BRAND,DESCRIPTION,QUANTITY,PRICE)
+VALUES
+--headphone
+('HEA','Shure AONIC 4','SHURE ','','20','8.800.000'),		
+('HEA','FiiO FH3','FIIO','','20','139.99'),
+('HEA','Klipsch T2 True Wireless','KLIPSCH','','20','80.68'),
+('HEA','Klipsch T5 II True Wireless','KLIPSCH','','20','6.500.000'),
+('HEA','Campfire Ara','CAMPFIRE AUDIO','','20','31.500.000'),
+('HEA','Campfire Solaris 2020','CAMPFIRE AUDIO','','20','36.000.000'),
+--monitor (sua nhe)
+('MON','LG 27GL83A-B','LG','27 Inch Ultragear QHD IPS 1ms NVIDIA G-SYNC Compatible Gaming Monitor, Black',20,379.99),
+('MON','Acer SB220Q bi','ACER','21.5 Inches Full HD (1920 x 1080) IPS Ultra-Thin Zero Frame Monitor (HDMI & VGA Port), Black',20,93.99),
+('MON','Sceptre 35 Inch Curved UltraWide 21','Sceptre','9 LED Creative Monitor QHD 3440x1440 Frameless AMD Freesync HDMI DisplayPort Up to 100Hz, Machine Black 2020',20,379.97),
+--keyboard
+('KEY','Azio Retro Classic Bluetooth','Azio','Luxury Vintage Backlit Mechanical Keyboard, MK-RETRO-L-03B-US',20,219.99),
+('KEY','Kinesis Advantage2 Ergonomic','KINESIS','Ergonomic',20,323.90),
+('KEY','Majestouch Convertible 2 TKL','FILCO','Ergonomic',20,179.00),
+--laptop
+('MacBook Pro','LAP','Apple MacBook Pro','Apple','16-inch, 16GB RAM, 512GB Storage',20,2249.00),	
+('LAP','ASUS F512DA-EB51 VivoBook','ASUS','15 Thin And Light Laptop, 15.6” Full HD, AMD Quad Core R5-3500U CPU, 8GB DDR4 RAM, 256GB PCIe SSD, AMD Radeon Vega 8 Graphics, Windows 10 Home,Slate Gray',20,589.00),
+('LAP','Acer Predator Helios 300 PH315-53-71VG','Acer','Intel i7-10750H, NVIDIA GeForce RTX 2070 Max-Q 8GB, 15.6" FHD 240Hz 3ms IPS, 16GB Dual-Channel DDR4, 512GB NVMe SSD, 1TB HDD, WiFi 6, RGB KB',20,1999.00),
+('LAP','Acer Predator Triton 500 PT515-52-73L3','Acer','Intel i7-10750H, NVIDIA GeForce RTX 2070 SUPER, 15.6" FHD NVIDIA G-SYNC Display, 300Hz, 16GB Dual-Channel DDR4, 512GB NVMe SSD, RGB Backlit KB',20,1659.99),
+('LAP','Acer Predator Triton 500 Thin & Light','Acer',' Intel Core i7-9750H, GeForce RTX 2060 with 6GB, 15.6" Full HD 144Hz 3ms IPS Display, 16GB DDR4, 512GB PCIe NVMe SSD, RGB Keyboard',20,1628.99),
+('LAP','ZenBook 15','ASUS','15.6” UHD 4K NanoEdge Display, Intel Core i7-10510U, GeForce GTX 1650, 16GB, 512GB PCIe SSD, ScreenPad 2.0, Amazon Alexa Compatible, Windows 10, Icicle Silver',20,1148.59),
+('LAP','Apple MacBook Pro','Apple','16-inch, 16GB RAM, 512GB Storage',20,2249.00),
