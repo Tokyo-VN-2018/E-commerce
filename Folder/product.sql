c5bf2cb5af6c8d3c864cee84ce38c5bce61ec24a
Nguyen Minh Hieu <75939521+kinglork932000@users.noreply.github.com>
2021-01-08 21:09:42 +0700

    Add files via upload

---
+++ Folder/product.sql
@@ -0,0 +1,17 @@
+USE ECOMMERCE;
+INSERT INTO PRODUCTS (CATEGORY_ID,PRODUCT_NAME,BRAND,DESCRIPTION,QUANTITY,PRICE)
+VALUES
+/*laptop*/
+('LAP','Dell Inspiron 15 G3 3590 N5I5517W','DELL'
+'CPU: Intel® Core™ i5-9300H, 8GB DDR4 2666MHz, 2 slot Ram, Max 32GB, 256GB SSD M.2 PCIe3x4, 
+NVIDIA Geforce GTX 1050 with 4GB GDDR5,15.6" FHD (1920 x 1080) IPS, Anti-Glare, Backlit full-sized chiclet keyboard (RGB Full-color),3-Cell 56WHrs,2.34Kg,Windows 10',100,867.43),
+('LAP','Apple MacBook Air 2017','APPLE','i5 1.8GHz/8GB/128GB0',100,824.04),
+('LAP','Acer Swift 3 SF315 52 38YQ','ACER','i3 8130U/4GB/1TB/Win10',100,409.39),
+('LAP','Acer Aspire A515 53 5112','ACER','i5 8265U/4GB+16GB/1TB/Win10',100,454.51),
+('LAP','Lenovo IdeaPad S340 14IIL','LENOVO','i5 1035G1/8GB/512GB/Win10',100,623.49),
+('LAP','HP 15s fq1111TU','HP','i3 1005G1/4GB/256GB/Win10',100,484.84),
+
+/*camera*/
+
+/*console*/
+('CON','PlayStation 5 Console','SONY','CPU x86-64 AMD "Jaguar",AMD R10xx 2,2GB RAM,RAM 8GB,USB 3.1(x2), HDMI out (x1)',100,499),
